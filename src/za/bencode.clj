4676c8c66b834280a1ea3763aa95c0f40a57d236
Zachary Allaun <zach.allaun@gmail.com>
2012-11-11 17:53:30 -0500

    Unfolololololold.
    As it happens, writing a reasonable docstring for unfold is particularly
    difficult for me.

--- src/za/bencode.clj
+++ src/za/bencode.clj
@@ -1,5 +1,19 @@
 (ns za.bencode)
 
+(defn unfold
+  "Accepts a predicate p, a function f, and an initial value x.
+
+  Unfold applies f to x and expects one of two kinds of values to be returned.
+  The first kind is some stop-unfold value that is discovered by passing the
+  value to p, which should return a truthy value when the unfold should stop.
+  The second is a [y new-x] pair, where y will be accumulated and ultimately
+  returned by the unfold, and new-x will be passed again into f to continue."
+  [p f x]
+  (lazy-seq
+    (let [result (f x)]
+      (when-not (p result)
+        (cons (first result) (unfold p f (second result)))))))
+
 (defn byteseq
   "Accepts a string, returns a sequence of bytes."
   [s]
--- test/za/bencode_test.clj
+++ test/za/bencode_test.clj
@@ -7,3 +7,21 @@
 
 (fact "sequence of bytes from a string"
       (byteseq "ascii") => (allfn seq? (partial every? byte?)))
+
+(fact "about simple unfold"
+      (unfold #(= % :stop-unfold)
+              (fn [xs]
+                (if (seq xs)
+                  [(first xs) (rest xs)]
+                  :stop-unfold))
+              [1 2 3])
+      => [1 2 3])
+
+(fact "about infinite unfold"
+      (take 5 (unfold #(= % :stop-unfold)
+                      (fn [xs]
+                        (if (seq xs)
+                          [(first xs) (rest xs)]
+                          :stop-unfold))
+                      (range)))
+      => [0 1 2 3 4])
