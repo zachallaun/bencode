cdef96b2806fbf6fbabb9084adf0221019ab2a44
Zachary Allaun <zach.allaun@gmail.com>
2012-11-11 18:07:08 -0500

    Stupid loop can be a map. Duh.

--- src/za/bencode.clj
+++ src/za/bencode.clj
@@ -17,7 +17,4 @@
 (defn byteseq
   "Accepts a string, returns a sequence of bytes."
   [s]
-  (lazy-seq
-    (loop [[c & more] s]
-      (when c
-        (cons (-> c int byte) (byteseq more))))))
+  (map #(-> % int byte) s))
