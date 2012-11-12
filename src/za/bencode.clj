7860118eeb0ffda52352935df589b0e85b235465
Zachary Allaun <zach.allaun@gmail.com>
2012-11-11 20:33:27 -0500

    Decoding for lists and integers.
    Stubs are in place for dictionaries and strings. (Necessary for
    implementing lists.)

--- src/za/bencode.clj
+++ src/za/bencode.clj
@@ -1,4 +1,5 @@
-(ns za.bencode)
+(ns za.bencode
+  (:refer-clojure :exclude [num list]))
 
 (defn unfold
   "TODO: unfold docstring"
@@ -13,3 +14,77 @@
   "Accepts a string, returns a sequence of bytes."
   [s]
   (map #(-> % int byte) s))
+
+(defn char<=
+  [& cs]
+  (apply <= (map int cs)))
+
+(defn end? [e] (= e \e))
+
+(defn read-chars
+  "Accepts a sequence of chars, concatenates them, and reads them.
+
+  [\1 \2 \3] => 123"
+  [chars]
+  (read-string (apply str chars)))
+
+(defn digit
+  [[x & rest]]
+  (when (and x (char<= \0 x \9))
+    [x rest]))
+
+(defn with-sign
+  [[x & rest :as bin]]
+  (if (or (= x \+) (= x \-))
+    [x rest]
+    [\+ bin]))
+
+(defn num
+  [bin]
+  (let [[sign bin] (with-sign bin)
+        [digits rest] (unfold nil? digit bin)]
+    (when (seq digits)
+      [(read-chars (cons sign digits)) rest])))
+
+(defmulti -decode
+  (fn [bin]
+    (first bin)))
+
+(defn decode
+  "Decodes a bencoded byte string.
+
+  \"i42e\" => 42
+  \"5:hello\" => \"hello\"
+  \"li42e3:fooe\" => (42, \"foo\")
+  \"d3:fooi1e3:bari2e\" => {\"foo\" 1 \"bar\" 2}"
+  [bin]
+  (-decode (map char bin)))
+
+(defn with-end
+  "Checks that unparsed characters start with char e, then returns the parse
+  result and the rest.
+
+  [parsed [e & rest]] => [parsed rest]
+  [parsed [not-e]] => nil"
+  [parser bin]
+  (when-let [[x [e & rest]] (parser bin)]
+    (when (end? e)
+      [x rest])))
+
+(defmethod -decode \i
+  [[_ & bin]]
+  (with-end num bin))
+
+(defn list
+  [bin]
+  (unfold nil? -decode bin))
+
+(defmethod -decode \l
+  [[_ & bin]]
+  (with-end list bin))
+
+(defmethod -decode \d
+  [[_ & bin]])
+
+(defmethod -decode :default
+  [[_ & bin]])
--- test/za/bencode_test.clj
+++ test/za/bencode_test.clj
@@ -16,3 +16,39 @@
                   :stop-unfold))
               [1 2 3])
       => [[1 2 3] []])
+
+(fact "about read-chars"
+      (read-chars [\1 \3 \3 \7]) => 1337
+      (read-chars [\- \1 \3 \3 \7]) => -1337
+      (read-chars [\a \b \c]) => 'abc)
+
+(fact "about with-sign"
+      (with-sign [\1]) => [\+ [\1]]
+      (with-sign [\- \1]) => [\- [\1]])
+
+(fact "about digit parsing"
+      (digit [\1 \f \o \o]) => [\1 [\f \o \o]]
+      (digit [\1]) => [\1 nil]
+      (digit [\f \o \o]) => nil?)
+
+(fact "about number parsing"
+      (num [\1 \2 \3 \f \o \o]) => [123 [\f \o \o]]
+      (num [\- \1 \2 \3 \f \o \o]) => [-123 [\f \o \o]]
+      (num [\f \1 \2 \3]) => nil?)
+
+(fact "about integer decoding"
+      (decode "i123e") => [123 nil]
+      (decode "i1337ei123e") => [1337 [\i \1 \2 \3 \e]]
+      (decode "i-1337e") => [-1337 nil])
+
+(fact "about poorly-formed integer decoding"
+      (decode "i123xe") => nil?)
+
+(fact "about list decoding"
+      (decode "li1ei2ei3ee") => [[1 2 3] nil]
+      (decode "li-1ei-2ei-3ee") => [[-1 -2 -3] nil]
+      (decode "li1ei2ei3eexxx") => [[1 2 3] [\x \x \x]])
+
+(fact "about poorly-formed list decoding"
+      (decode "li1ei2ei3exe") => nil?)
+
