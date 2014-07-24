f5e23fb235ba0dade12955e37f75ac1d9d8f831c
Zachary Allaun <zach.allaun@gmail.com>
2014-07-24 14:09:00 -0400

    Fix tests (testing -decode instead of decode)

--- test/za/bencode_test.clj
+++ test/za/bencode_test.clj
@@ -38,32 +38,32 @@
   (num [\f \1 \2 \3]) => nil?)
 
 (fact "about integer decoding"
-  (decode "i123e") => [123 nil]
-  (decode "i1337ei123e") => [1337 [\i \1 \2 \3 \e]]
-  (decode "i-1337e") => [-1337 nil])
+  (-decode "i123e") => [123 nil]
+  (-decode "i1337ei123e") => [1337 [\i \1 \2 \3 \e]]
+  (-decode "i-1337e") => [-1337 nil])
 
 (fact "about poorly-formed integer decoding"
-  (decode "i123xe") => nil?)
+  (-decode "i123xe") => nil?)
 
 (fact "about list decoding"
-  (decode "li1ei2ei3ee") => [[1 2 3] nil]
-  (decode "li-1ei-2ei-3ee") => [[-1 -2 -3] nil]
-  (decode "li1ei2ei3eexxx") => [[1 2 3] [\x \x \x]]
-  (decode "ld1:ai0eee") => [[{"a" 0}] nil])
+  (-decode "li1ei2ei3ee") => [[1 2 3] nil]
+  (-decode "li-1ei-2ei-3ee") => [[-1 -2 -3] nil]
+  (-decode "li1ei2ei3eexxx") => [[1 2 3] [\x \x \x]]
+  (-decode "ld1:ai0eee") => [[{"a" 0}] nil])
 
 (fact "about poorly-formed list decoding"
-  (decode "li1ei2ei3exe") => nil?)
+  (-decode "li1ei2ei3exe") => nil?)
 
 (fact "about string decoding"
-  (decode "3:foo") => ["foo" ()]
-  (decode "3:foobar") => ["foo" [\b \a \r]])
+  (-decode "3:foo") => ["foo" ()]
+  (-decode "3:foobar") => ["foo" [\b \a \r]])
 
 (fact "about poorly-formed string decoding"
-  (decode "3x:foo") => nil?)
+  (-decode "3x:foo") => nil?)
 
 (fact "about dictionary decoding"
-  (decode "d1:ai0e1:bi1ee") => [{"a" 0 "b" 1} nil]
-  (decode "d1:ali0ei1ei2eee") => [{"a" [0 1 2]} nil])
+  (-decode "d1:ai0e1:bi1ee") => [{"a" 0 "b" 1} nil]
+  (-decode "d1:ali0ei1ei2eee") => [{"a" [0 1 2]} nil])
 
 (fact "about string encoding"
   (-encode "foobar") => (map char->byte "6:foobar")
