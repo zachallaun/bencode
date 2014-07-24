47436557ff4f17bb326af696acf90b3779e861eb
Zachary Allaun <zach.allaun@gmail.com>
2014-07-24 14:05:10 -0400

    Upgrade to most recent clojure/midje/etc.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject za.bencode "0.1.0-SNAPSHOT"
   :description "Bencode encoding and decoding."
-  :url ""
+  :url "https://github.com/zachallaun/bencode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
+                   :dependencies [[midje "1.6.3"]]}})
