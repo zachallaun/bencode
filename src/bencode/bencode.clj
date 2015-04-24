89617d44e24d4e790ad8c578ca4115d4152b7bcc
Pietro F. Menna <pietromenna@yahoo.com>
2015-04-24 12:37:27 -0400

    Removed dependency on Flatland

--- src/bencode/bencode.clj
+++ src/bencode/bencode.clj
@@ -1,6 +1,5 @@
 (ns bencode.bencode
-  (:refer-clojure :exclude [num list])
-  (:require [flatland.ordered.map :refer [ordered-map]]))
+  (:refer-clojure :exclude [num list]))
 
 (defn file-stream
   [fname]
@@ -110,7 +109,7 @@
 (defmethod -decode \d
   [[_ & bin]]
   (when-let [[dict more] (with-end #(unfold (complement seq) kv %) bin)]
-    [(into (ordered-map) dict) more]))
+    [(into {} dict) more]))
 
 (defn decode
   "Decodes a bencoded sequence in ISO-8859-1 format. If passing in a
